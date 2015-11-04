076168fad049ca84dbb0ca0cb8005ceb8bc5d9b4
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-11-04 19:11:37 +0100

    One-to-one fixed

--- Student_Cuda_Image/src/cpp/core/01_Rippling/moo/device/ripplingDevice.cu
+++ Student_Cuda_Image/src/cpp/core/01_Rippling/moo/device/ripplingDevice.cu
@@ -1,7 +1,7 @@
 #include <iostream>
 
-#include "Indice1D.h"
 #include "cudaTools.h"
+#include "Indice2D.h"
 #include "Device.h"
 #include "IndiceTools.h"
 #include "RipplingMath.h"
@@ -10,7 +10,7 @@ __global__ void rippling(uchar4* ptrDevPixels, int w, int h, float t);
 
 __global__ void rippling(uchar4* ptrDevPixels, int w, int h, float t) {
 
-// ==== ONE-TO-ONE ====
+// ==== Entrelacement ====
   // RipplingMath ripplingMath = RipplingMath(w, h);
   //
   // const int NB_THREADS = Indice2D::nbThread();
@@ -26,25 +26,26 @@ __global__ void rippling(uchar4* ptrDevPixels, int w, int h, float t) {
 
 
 // ==== ONE-TO-ONE ====
-  // RipplingMath ripplingMath = RipplingMath(w, h);
-  //
-  // int i = threadIdx.y + blockIdx.y * blockDim.y;
-  // int j = threadIdx.x + blockIdx.x * blockDim.x;
-  // int s = j + gridDim.x * (threadIdx.y + blockIdx.y * blockDim.y);
-  //
-  // ripplingMath.colorIJ(&ptrDevPixels[s], i, j, t);
-
-// ==== One dimension ====
   RipplingMath ripplingMath = RipplingMath(w, h);
 
-  const int NB_THREADS = Indice1D::nbThread();
-  const int TID = Indice1D::tid();
-  const int n = w * h;
-  int s = TID;
-  while( s < n ) {
-    int i, j;
-    IndiceTools::toIJ(s, w, &i, &j);
-    ripplingMath.colorIJ(&ptrDevPixels[s], i, j, t);
-    s += NB_THREADS;
-  }
+  int i = threadIdx.y + blockIdx.y * blockDim.y;
+  int j = threadIdx.x + blockIdx.x * blockDim.x;
+  int s = j + gridDim.x * blockDim.x * (threadIdx.y + blockIdx.y * blockDim.y);
+
+  ripplingMath.colorIJ(&ptrDevPixels[s], i, j, t);
+
+
+// ==== One dimension ====
+  // RipplingMath ripplingMath = RipplingMath(w, h);
+  //
+  // const int NB_THREADS = Indice1D::nbThread();
+  // const int TID = Indice1D::tid();
+  // const int n = w * h;
+  // int s = TID;
+  // while( s < n ) {
+  //   int i, j;
+  //   IndiceTools::toIJ(s, w, &i, &j);
+  //   ripplingMath.colorIJ(&ptrDevPixels[s], i, j, t);
+  //   s += NB_THREADS;
+  // }
 }
