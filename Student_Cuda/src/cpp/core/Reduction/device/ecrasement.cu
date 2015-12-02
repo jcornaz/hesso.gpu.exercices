e8bdd17fc47a8d34df3581fc433173d7efd12ce5
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-12-02 19:11:17 +0100

    Add inter-block reduction and indices computes

--- Student_Cuda/src/cpp/core/Reduction/device/ecrasement.cu
+++ Student_Cuda/src/cpp/core/Reduction/device/ecrasement.cu
@@ -2,24 +2,35 @@
 #include "cudaTools.h"
 
 __device__ void ecrasement(float* arraySM, int size) {
+  // Intra-block
 
-  // Pattern local au bloc
-  const int NB_THREAD=Indice2D::nbThread();
-  const int TID=Indice2D::tid();
+  const int NB_THREAD_LOCAL = blockDim.x * blockDim.y;
+  const int TID_LOCAL = blockId.x + blockId.y * blockDim.x;
 
   int n = size;
   int half = size / 2;
   while (half >= 1) {
 
-    int s = TID;
+    int s = TID_LOCAL;
     while (s < half) {
       arraySM[s] += arraySM[s + half];
-      s += NB_THREAD;
+      s += NB_THREAD_LOCAL;
     }
 
     __syncthreads();
 
     n = hafl;
     half = n / 2;
+  }
+}
+
+__device__ void interbloc(float* arraySM, int size, float* resultGM) {
+  // inter-block
+
+  const int NB_THREAD_LOCAL = blockDim.x * blockDim.y;
+  const int TID_LOCAL = blockId.x + blockId.y * blockDim.x;
+
+  if (TID_LOCAL == 0) {
+    atomicAdd(resultGM, arraySM[0]);
   }
 }
