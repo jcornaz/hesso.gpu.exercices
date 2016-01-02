611ae7cf51ead9a6ded1fa2718d347ddce326261
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2016-01-02 15:37:11 +0100

    Implement a mutli-GPU version of monte carlo

--- Student_Cuda/src/cpp/core/04_MonteCarlo/monte_carlo_host.cu
+++ Student_Cuda/src/cpp/core/04_MonteCarlo/monte_carlo_host.cu
@@ -7,6 +7,7 @@
 extern __global__ void computePIWithMonteCarlo(int* ptrDevResult, curandState* ptrDevTabGenerators, int nbGen);
 
 bool isMonteCarloOk();
+bool isMonteCarloMultiGPUOk();
 
 bool isMonteCarloOk() {
 
@@ -14,8 +15,8 @@ bool isMonteCarloOk() {
   const int NB_THREADS_BY_BLOCK = 512;
   const int NB_GENERATIONS = 1000000;
 
-  dim3 dg = dim3(NB_BLOCS, 1, 1);
-  dim3 db = dim3(NB_THREADS_BY_BLOCK, 1, 1);
+  dim3 dg(NB_BLOCS, 1, 1);
+  dim3 db(NB_THREADS_BY_BLOCK, 1, 1);
 
   Device::assertDim(dg, db);
 
@@ -37,6 +38,50 @@ bool isMonteCarloOk() {
   float piValue = nbPointsOnIntegralArea * 4.0 / NB_GENERATIONS;
 
   std::cout << "PI = " << piValue << " (with Monte Carlo)" << std::endl;
+
+  return abs(piValue - 3.141592653589793f) < 0.001;
+}
+
+bool isMonteCarloMultiGPUOk() {
+
+  const int NB_BLOCS = 128;
+  const int NB_THREADS_BY_BLOCK = 512;
+  const int NB_GENERATIONS_BY_DEVICE = 1000000;
+
+  int nbDevice = Device::getDeviceCount();
+
+  int deviceSums[nbDevice];
+
+  #pragma omp parallel for
+  for (int deviceID = 0 ; deviceID < nbDevice ; deviceID++) {
+    dim3 dg(NB_BLOCS, 1, 1);
+    dim3 db(NB_THREADS_BY_BLOCK, 1, 1);
+
+    Device::assertDim(dg, db);
+
+    int* ptrDevResult;
+    curandState* ptrDevTabGenerators;
+
+    HANDLE_ERROR(cudaMalloc(&ptrDevResult, sizeof(int)));
+    HANDLE_ERROR(cudaMalloc(&ptrDevTabGenerators, NB_BLOCS * NB_THREADS_BY_BLOCK + sizeof(curandState)));
+    HANDLE_ERROR(cudaMemset(ptrDevResult, 0, sizeof(int)));
+
+    setup_kernel_rand<<<dg,db>>>(ptrDevTabGenerators, deviceID);
+    computePIWithMonteCarlo<<<dg,db>>>(ptrDevResult, ptrDevTabGenerators, NB_GENERATIONS_BY_DEVICE);
+
+    HANDLE_ERROR(cudaMemcpy(&deviceSums[deviceID], ptrDevResult, sizeof(int), cudaMemcpyDeviceToHost));
+    HANDLE_ERROR(cudaFree(ptrDevResult));
+    HANDLE_ERROR(cudaFree(ptrDevTabGenerators));
+  }
+
+  int finalSum = 0;
+  for (int i = 0 ; i < nbDevice ; i++) {
+    finalSum += deviceSums[0];
+  }
+
+  float piValue = finalSum * 4.0 / (nbDevice * NB_GENERATIONS_BY_DEVICE);
+
+  std::cout << "PI = " << piValue << " (with Monte Carlo multi GPU)" << std::endl;
 
   return abs(piValue - 3.141592653589793f) < 0.001;
 }
--- Student_Cuda/src/cpp/core/04_MonteCarlo/useMonteCarlo.cpp
+++ Student_Cuda/src/cpp/core/04_MonteCarlo/useMonteCarlo.cpp
@@ -1,7 +1,13 @@
 extern bool isMonteCarloOk();
+extern bool isMonteCarloMultiGPUOk();
 
 bool useMonteCarlo();
+bool useMonteCarloMultiGPU();
 
 bool useMonteCarlo() {
   return isMonteCarloOk();
+}
+
+bool useMonteCarloMultiGPU() {
+  return isMonteCarloMultiGPUOk();
 }
